ae30d52a2cf8cb841707cd42fac6b79de9a7d7cc
Chandra0505 <chandrayadav0505@gmail.com>
2019-03-13 23:45:41 +0530

    deleted some useless files from admin

---
+++ info.php
@@ -0,0 +1,7 @@
+<?php 
+//echo "GD: ", extension_loaded('gd') ? 'OK' : 'MISSING', '<br>';
+//echo "XML: ", extension_loaded('xml') ? 'OK' : 'MISSING', '<br>';
+//echo "zip: ", extension_loaded('zip') ? 'OK' : 'MISSING', '<br>';
+
+phpinfo();
+?>
